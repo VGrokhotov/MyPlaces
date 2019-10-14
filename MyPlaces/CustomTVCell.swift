c911090ac506111c4843b6402625109d55e2ec6b
Vladislav <vlad@grokhotov.ru>
2019-10-14 19:18:37 +0300

    custom table view cell

---
+++ MyPlaces/CustomTVCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTVCell.swift
+//  MyPlaces
+//
+//  Created by Владислав on 14/10/2019.
+//  Copyright © 2019 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class CustomTVCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
