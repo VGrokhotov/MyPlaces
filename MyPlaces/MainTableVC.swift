56cd9550ceaa01425f701be0c47a5e03a703d024
Vladislav <vlad@grokhotov.ru>
2019-10-14 17:28:55 +0300

    initial commit

---
+++ MyPlaces/MainTableVC.swift
@@ -0,0 +1,42 @@
+//
+//  MainTableVC.swift
+//  MyPlaces
+//
+//  Created by Владислав on 13/10/2019.
+//  Copyright © 2019 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class MainTableVC: UITableViewController {
+    let restaurantNames = [ "Butcher", "Delmar", "Macdonalds", "Burger King", "Hesburger", "KFC", "Кофейня №1"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
