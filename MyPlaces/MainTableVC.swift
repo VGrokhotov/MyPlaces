2edf7af2354ef06e1e9ba8f1839f48d68910953c
Vladislav <vlad@grokhotov.ru>
2019-10-16 19:18:13 +0300

    delete of records added

--- MyPlaces/MainTableVC.swift
+++ MyPlaces/MainTableVC.swift
@@ -44,7 +44,16 @@ class MainTableVC: UITableViewController {
         return cell
     }
     
+    // MARK: - Table viev delegate
     
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let place = places[indexPath.row]
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (_, _) in
+            StorageManager.deleteObject(place)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        return [deleteAction]
+    }
 
     /*
     // MARK: - Navigation
